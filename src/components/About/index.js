44dd20d70f88c8637a87156844719bdb1c5b14ec
Reilly Catrambone <reillycatrambone@Reillys-MacBook-Pro.local>
2022-12-10 18:23:00 -0800

    Added an About page

---
+++ src/components/About/index.js
@@ -0,0 +1,24 @@
+import './index.scss'
+
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    About Me
+                </h1>
+
+                <p> 
+                    I am an engineer looking to solve difficult problems and build quality products.
+                </p>
+
+                <p> 
+                    I graduated from UC Berkeley with a Bachelors in Data Science, 
+                    primarily focused in Finance.    
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
