7ec8ecf18883f74a5b495113763ddfe8a119d8e2
Reilly Catrambone <reillycatrambone@Reillys-MacBook-Pro.local>
2022-12-10 18:54:42 -0800

    Added cube spinner

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,5 @@
+import { faCss3, faGitAlt, faHtml5, faJava, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './index.scss'
 
 const About = () => {
@@ -17,6 +19,29 @@ const About = () => {
                     primarily focused in Finance.    
                 </p>
             </div>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'>
+                        <FontAwesomeIcon icon={faPython} color='#DD0031'/>
+                    </div>
+                    <div className='face2'>
+                        <FontAwesomeIcon icon={faReact} color='#7FFFD4'/>
+                    </div>
+                    <div className='face3'>
+                        <FontAwesomeIcon icon={faJava} color='#FF7F50'/>
+                    </div>
+                    <div className='face4'>
+                        <FontAwesomeIcon icon={faHtml5} color='#FFD700'/>
+                    </div>
+                    <div className='face5'>
+                        <FontAwesomeIcon icon={faGitAlt} color='#BA55D3'/>
+                    </div>
+                    <div className='face6'>
+                        <FontAwesomeIcon icon={faCss3} color='#98FB98'/>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
